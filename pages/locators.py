4b837e5b98fec442990d6835b3f05ae79c1b3abe
AiratZa <zalyaliev.evermore@gmail.com>
2020-04-12 13:21:29 +0300

    Parametrized test with xfail

--- pages/locators.py
+++ pages/locators.py
@@ -11,3 +11,10 @@ class LoginPageLocators():
     PASSWORD_FORM = (By.NAME, "login-password")
     SUBMIT_BTN = (By.NAME, "login-submit")
     SUBMIT_BTN = (By.LINK_TEXT, "Я забыл пароль")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CLASS_NAME, "btn-add-to-basket")
+    SUCCESS_ADDED_ITEM_ALERT = (By.CSS_SELECTOR, "#messages>.alert:nth-child(1)>.alertinner>strong") 
+    BASKET_TOTAL_COST_ALERT = (By.CSS_SELECTOR, "#messages>.alert:nth-child(3)>.alertinner>p>strong") 
+    ADDING_ITEM_NAME = (By.CSS_SELECTOR, ".product_main>h1") 
+    ADDING_ITEM_COST = (By.CSS_SELECTOR, ".product_main>.price_color") 
